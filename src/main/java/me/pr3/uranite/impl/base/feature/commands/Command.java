b250a185a23eb85dbea12ceaa3f2dd62fb354787
tim <pr3roxdlc.yt@web.de>
2023-01-24 15:09:45 +0100

    Add defaults for Command and Module Annotations

--- src/main/java/me/pr3/uranite/impl/base/feature/commands/Command.java
+++ src/main/java/me/pr3/uranite/impl/base/feature/commands/Command.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 public @interface Command {
     String name();
-    String syntax();
-    String description();
+    String syntax() default "";
+    String description() default "";
 }
--- src/main/java/me/pr3/uranite/impl/base/feature/modules/Module.java
+++ src/main/java/me/pr3/uranite/impl/base/feature/modules/Module.java
@@ -10,5 +10,5 @@ import java.lang.annotation.Target;
 public @interface Module {
     String name();
     String category();
-    String description();
+    String description() default "";
 }
